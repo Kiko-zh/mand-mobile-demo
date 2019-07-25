a5dd0b10dabaf2f2756e80a40459306bbe064bf2
Kiki7792 <921970613@qq.com>
2019-07-25 13:02:15 +0800

    初始化

---
+++ src/components/VxHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="md-example-item-s">
+      <md-icon name="location" size="md"></md-icon>
+      <p>md</p>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import {Icon} from 'mand-mobile'
+export default {
+  name: '',
+  mixins: [],
+  components: {
+    [Icon.name]: Icon
+  },
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  filters: {},
+  mounted () { },
+  created () { }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
